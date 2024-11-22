c12872d9b9a0b844c6eb4e348b1b22e8db26def0
Ritish Nagpal <147669389+Ritishnagpal@users.noreply.github.com>
2024-11-22 13:23:44 +0530

    Update auth_provider.dart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -8,10 +8,9 @@ class AuthProvider with ChangeNotifier {
 
   User? get currentUser => _firebaseAuth.currentUser;
 
-  // Sign in with Email/Password
   Future<Map<String, dynamic>?> signInWithEmailPassword(String email, String password) async {
     try {
-      // Sign in with email and password
+   
       final UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(
         email: email,
         password: password,
@@ -19,7 +18,7 @@ class AuthProvider with ChangeNotifier {
 
       final uid = userCredential.user!.uid;
 
-      // Fetch the user data from Firestore
+    
       final userDoc = await FirebaseFirestore.instance.collection('users').doc(uid).get();
 
       if (userDoc.exists) {
@@ -33,86 +32,72 @@ class AuthProvider with ChangeNotifier {
   }
 
 
-  // Register with Email/Password
   Future<void> registerWithEmailPassword(String name, String email, String password) async {
     try {
-      // Create the user and capture the UserCredential
+    
       final UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
 
-      // Extract the user ID
+  
       final uid = userCredential.user!.uid;
 
-      // Print the UID to the console
+     
       print("User ID: $uid");
 
-      // Save user data in Firestore
+    
       await FirebaseFirestore.instance.collection('users').doc(uid).set({
         'uid': uid,
         'name': name,
         'email': email,
         'createdAt': FieldValue.serverTimestamp(),
       });
-
-      // Notify listeners if using a state management solution
       notifyListeners();
     } catch (e) {
       throw Exception("Failed to register: ${e.toString()}");
     }
   }
-
-
-
-  // Sign in with Google
   Future<void> signInWithGoogle() async {
     try {
       final GoogleSignIn googleSignIn = GoogleSignIn();
       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 
       if (googleUser == null) {
-        // User canceled the sign-in.
+    
         print("Sign-in aborted by user.");
         return;
       }
-
       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
-
-      // Sign in to Firebase using the credential
       final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
       final User? user = userCredential.user;
 
       if (user != null) {
         print("Google sign-in successful");
-
-        // Check if the user exists in Firestore
         final userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
 
         if (!userDoc.exists) {
-          // If the user doesn't exist, create a new profile
           await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
             'email': user.email,
             'name': user.displayName,
-            'profileImage': user.photoURL ?? '', // Set the profile image if available
+            'profileImage': user.photoURL ?? '', 
             'createdAt': FieldValue.serverTimestamp(),
           });
 
           print("New user profile created in Firestore");
         } else {
-          // User already exists in Firestore, update if necessary
+         
           print("User profile already exists in Firestore");
         }
 
       }
     } catch (e) {
       print("Error during Google sign-in: $e");
-      // Handle errors appropriately in your UI
+  
     }
   }
 
